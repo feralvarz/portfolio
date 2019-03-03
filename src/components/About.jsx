e382ca930c8c5d9cdb2252a1d5053934e02fa4ed
Fernando Alvarez <fernando.alvarez@centricdigital.com>
2019-03-03 18:19:26 -0300

    Layout updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,69 +4,69 @@ import profile_img from "../assets/images/fernando-alvarez-profile.jpg";
 class About extends Component {
   render() {
     return (
-      <div className="about">
-        <div className="container h-100">
-          <div className="content h-100">
-            <div className="row h-100 align-items-center">
-              <div className="col-lg-10 mx-auto">
-                <div className="row mb-4">
-                  <div className="col-lg-5 col-xl-3">
-                    <img
-                      className="img-fluid mb-2 shadow"
-                      src={profile_img}
-                      alt="Fernando Alvarez web developer"
-                    />
-                  </div>
+      <div className="about container">
+        <div className="row h-100 align-items-center">
+          <div className="col-lg-10 mx-auto">
+            <div className="row mb-4">
+              <div className="col-md-3">
+                <img
+                  className="img-fluid mb-2 shadow"
+                  src={profile_img}
+                  alt="Fernando Alvarez web developer"
+                />
+              </div>
 
-                  <div className="col-lg-5 col-xl-4 pl-4 soffset-lg-1">
-                    <h2 className="h5">About</h2>
-                    <p>
-                      In the past 11 years, Fernando was part of a wide range of
-                      projects from Musicians, Non-profits, Start-ups, and
-                      Corporations being the use of best practices and the
-                      latest tools the base of his work process.
-                    </p>
+              <div className="col-md-6">
+                <h2 className="h5">About</h2>
+                <p>
+                  In the past 11 years, Fernando was part of a wide range of
+                  projects from Musicians, Non-profits, Start-ups, and
+                  Corporations being the use of best practices and the latest
+                  tools the base of his work process.
+                </p>
 
-                    <p>
-                      He believes in projects that he is passionate about,
-                      always providing a direct benefit for his clients.
-                    </p>
-                  </div>
-                </div>
-
-                <div className="row">
-                  <div className="col-xl-8">
-                    <div className="row">
-                      <div className="col">
-                        <h2 className="h6 mb-1">Skills</h2>
-                        <p className="small">
-                          CSS3, HTML5, Javascript, React, Angular, Wordpress,
-                          Highcharts, D3.js, Bootstrap, Adobe Suite, Sketch,
-                          Responsive Web Design, Responsive Web Development, UI
-                          Development.
-                        </p>
-                      </div>
-                      <div className="col">
-                        <h2 className="h6 mb-1">Clients</h2>
-                        <p className="small">
-                          AIG®, Kaiser Permanente® , Humana™, The Leukemia &
-                          Lymphoma Society®, Andrew McMahon in the Wilderness,
-                          Dearjack Foundation, JD Power.
-                        </p>
-                      </div>
-                    </div>
-                    <hr />
-                  </div>
-                </div>
-
-                <p className="small mb-0">
-                  <strong className="mr-1">Contact: </strong>
-                  <a href="mailto:feralvarz@gmail.com" target="_blank">
-                    feralvarz@gmail.com
-                  </a>
+                <p>
+                  He believes in projects that he is passionate about, always
+                  providing a direct benefit for his clients.
                 </p>
               </div>
             </div>
+
+            <div className="row">
+              <div className="col-xl-8">
+                <div className="row">
+                  <div className="col">
+                    <h2 className="h6 mb-1">Skills</h2>
+                    <p className="small">
+                      CSS3, HTML5, Javascript, React, Angular, Wordpress,
+                      Highcharts, D3.js, Bootstrap, Adobe Suite, Sketch,
+                      Responsive Web Design, Responsive Web Development, UI
+                      Development.
+                    </p>
+                  </div>
+                  <div className="col">
+                    <h2 className="h6 mb-1">Clients</h2>
+                    <p className="small">
+                      AIG®, Kaiser Permanente® , Humana™, The Leukemia &
+                      Lymphoma Society®, Andrew McMahon in the Wilderness,
+                      Dearjack Foundation, JD Power.
+                    </p>
+                  </div>
+                </div>
+                <hr />
+              </div>
+            </div>
+
+            <p className="small mb-0">
+              <strong className="mr-1">Contact: </strong>
+              <a href="mailto:feralvarz@gmail.com" target="_blank">
+                feralvarz@gmail.com
+              </a>
+            </p>
+
+            <p className="small pt-2">
+              <small>Fernando Alvarez Portfolio - Made with React</small>
+            </p>
           </div>
         </div>
       </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,26 +16,13 @@ class Navigation extends Component {
             <Link to="/" className="">
               Fernando Alvarez
             </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav w-100">
-                <li className="nav-item ml-auto">
-                  <Link to="/about" className="nav-link">
-                    About
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
+              </li>
+            </ul>
           </nav>
         </header>
       </div>
